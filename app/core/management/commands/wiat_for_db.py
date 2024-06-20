068ab0b20646219851fdb13ae41da0d952eb5f1a
https://github.com/siangit/homework.git <ljin6956@gmail.com>
2024-06-20 13:58:17 +0900

    docker_compose revise for wait_for_db

---
+++ app/core/management/commands/wiat_for_db.py
@@ -0,0 +1,25 @@
+# wait_for_db ==>django가 db가 준비될때까지 연결을 재시도하기위해 필요
+#하나의 docker image에 각 컨테이너(app, db)가 존재하기 때문
+
+# Django가 DB 연결에 실패했을 시, 재시도 하도록 만드는 로직을 추가
+from django.core.management.base import BaseCommand
+from django.db import connections
+import time
+
+# Operation Error & Psycopg2 Operation Error
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2OPsycopgpError
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write('Wating for DB connection ...')
+
+        is_db_connected = None
+        while not is_db_connected:
+            try:
+                is_db_connected = connections['default']
+            except (OperationalError, Psycopg2OPsycopgpError):
+                self.stdout.write("Retrying DB connection ...")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Congratue! PostgreSQL Connection Success!!!"))
